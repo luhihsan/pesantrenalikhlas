e6362f1f94331db6c9272aeffb86ce2ffe178085
dxAryo <gilang91668@gmail.com>
2023-08-02 00:10:05 +0700

    maps

--- views/admin/index.php
+++ views/admin/index.php
@@ -147,7 +147,7 @@
                 </div>
                 <div class="col text-center">
                     <h4>ADDRESS</h4>
-                    <p> Bulu, Jogotirto, Kec. Berbah, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55573</p>
+                    <a href="https://goo.gl/maps/ZKVn3DQBXtZuk9Sq7" class="text-light">Bulu, Jogotirto, Kec. Berbah, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55573</a>
                 </div>
                 <div class="col text-center">
                     <h4>NAVIGATE</h4>
