a8a383e26cb91c8185fac7bb5dc9a6f2a9e959d8
dxAryo <gilang91668@gmail.com>
2023-08-01 22:52:18 +0700

    profile

--- views/pendaftar/notsetprofile.php
+++ views/pendaftar/notsetprofile.php
@@ -4,79 +4,35 @@
     <?php
         include "../../controller/connect.php";
         $id = $_SESSION['id'];
-        $query_status = mysqli_query($connect, "SELECT * FROM pendaftaranawal WHERE id='$id'");
+        $query_status = mysqli_query($connect, "SELECT * FROM user WHERE id='$id'");
         $data_status = mysqli_fetch_assoc($query_status);
 
-        $query_profil = "SELECT * FROM pendaftaranawal";
-        $result_profil = mysqli_query($connect, $query_profil);
-        $row_profil = mysqli_fetch_assoc($result_profil);
     ?>
     <div class="row">
         <div class="col-3">
-            <img src="../../assets/img/<?= $row_profil['foto'] ?>" alt="" width="200">
+            <!-- <img src="../../assets/img/<?= $data_status['foto'] ?>" alt="" width="200"> -->
         </div>
         <div class="col">
             <table class="table">
                 <tr>
-                    <td>Nama lengkap</td>
+                    <td>Nama lengkapss</td>
                     <td>:</td>
-                    <td><?php echo isset($row_profil['nama']) ? $row_profil['nama'] : 'N/A'; ?></td>
+                    <td><?php echo isset($data_status['nama']) ? $data_status['nama'] : 'N/A'; ?></td>
                 </tr>
                 <tr>
-                    <td>Tempat Lahir</td>
+                    <td>Email</td>
                     <td>:</td>
-                    <td><?php echo isset($row_profil['tempat_lahir']) ? $row_profil['tempat_lahir'] : 'N/A'; ?></td>
+                    <td><?php echo isset($data_status['email']) ? $data_status['email'] : 'N/A'; ?></td>
                 </tr>
-                <tr>
-                    <td>Tanggal Lahir</td>
-                    <td>:</td>
-                    <td><?php echo isset($row_profil['tgllhr']) ? $row_profil['tgllhr'] : 'N/A'; ?></td>
-                </tr>
-                <tr>
-                    <td>NISN</td>
-                    <td>:</td>
-                    <td><?php echo isset($row_profil['nisn']) ? $row_profil['nisn'] : 'N/A'; ?></td>
-                </tr>
-                <tr>
-                    <td>Asal Sekolah</td>
-                    <td>:</td>
-                    <td><?php echo isset($row_profil['asal_sekolah']) ? $row_profil['asal_sekolah'] : 'N/A'; ?></td>
-                </tr>
-                <tr>
-                    <td>Alamat</td>
-                    <td>:</td>
-                    <td><?php echo isset($row_profil['alamat']) ? $row_profil['alamat'] : 'N/A'; ?></td>
-                </tr>
-                <tr>
-                    <td>Nama Ayah</td>
-                    <td>:</td>
-                    <td><?php echo isset($row_profil['nama_ayah']) ? $row_profil['nama_ayah'] : 'N/A'; ?></td>
-                </tr>
-                <tr>
-                    <td>Nama Ibu</td>
-                    <td>:</td>
-                    <td><?php echo isset($row_profil['nama_ibu']) ? $row_profil['nama_ibu'] : 'N/A'; ?></td>
-                </tr>
-                <tr>
-                    <td>Program Yang Diambil</td>
-                    <td>:</td>
-                    <td><?php echo isset($row_profil['program']) ? $row_profil['program'] : 'N/A'; ?></td>
-                </tr>
+                
                 <tr>
                     <td>Status Pendaftaran</td>
                     <td>:</td>
                     <td>
                     <?php 
-                        if ($row_profil['status'] == '1') {
-                            echo "<span class='badge badge-success'>Selamat Anda Diterima Sebagai Calon Santri</span>";
-                            echo "<br>";
-                            echo "<br>";
-                            echo "<a href='../../controller/generate_pdf.php?no_pendfawal=$row_profil[no_pendfawal]' target='_blank' class='btn btn-info'>Cetak PDF</a>";
-                        } else if  ($row_profil['status'] == '2'){
-                            echo "<span class='badge badge-danger'>Mohan Maaf, Anda Tidak Lolos Seleksi Sebagai Calon Santri</span>";
-                        } else {
-                            echo "<div class='alert alert-warning' role='alert'>Status : Sedang Dalam Proses Penilaian Panitia</div>";
-                        }   
+                       echo "<div class='alert alert-warning' role='alert'>Status : Daftarkan diri anda!</div>";
+                       
+                       
                     ?>
                     </td>
                 </tr>
--- views/pendaftar/profile.php
+++ views/pendaftar/profile.php
@@ -4,53 +4,80 @@
     <?php
         include "../../controller/connect.php";
         $id = $_SESSION['id'];
-        $query = mysqli_query($connect, "SELECT * FROM pendaftaranawal WHERE id='$id'");
-        $data = mysqli_fetch_assoc($query);
+        $query_status = mysqli_query($connect, "SELECT * FROM pendaftaranawal WHERE id='$id'");
+        $data_status = mysqli_fetch_assoc($query_status);
+
     ?>
     <div class="row">
         <div class="col-3">
-            <img src="../../assets/img/<?= $data['foto'] ?>" alt="" width="200">
+            <img src="../../assets/img/<?= $data_status['foto'] ?>" alt="" width="200">
         </div>
         <div class="col">
             <table class="table">
                 <tr>
-                    <td>Nama lengkap</td>
+                    <td>Nama lengkapaa</td>
                     <td>:</td>
-                    <td><?= $data['nama'] ?></td>
+                    <td><?php echo isset($data_status['nama']) ? $data_status['nama'] : 'N/A'; ?></td>
                 </tr>
                 <tr>
                     <td>Tempat Lahir</td>
                     <td>:</td>
-                    <td><?= $data['tempat_lahir'] ?></td>
+                    <td><?php echo isset($data_status['tempat_lahir']) ? $data_status['tempat_lahir'] : 'N/A'; ?></td>
+                </tr>
+                <tr>
+                    <td>Tanggal Lahir</td>
+                    <td>:</td>
+                    <td><?php echo isset($data_status['tgllhr']) ? $data_status['tgllhr'] : 'N/A'; ?></td>
                 </tr>
                 <tr>
                     <td>NISN</td>
                     <td>:</td>
-                    <td><?= $data['nisn'] ?></td>
+                    <td><?php echo isset($data_status['nisn']) ? $data_status['nisn'] : 'N/A'; ?></td>
                 </tr>
                 <tr>
                     <td>Asal Sekolah</td>
                     <td>:</td>
-                    <td><?= $data['asal_sekolah'] ?></td>
+                    <td><?php echo isset($data_status['asal_sekolah']) ? $data_status['asal_sekolah'] : 'N/A'; ?></td>
                 </tr>
                 <tr>
                     <td>Alamat</td>
                     <td>:</td>
-                    <td><?= $data['alamat'] ?></td>
+                    <td><?php echo isset($data_status['alamat']) ? $data_status['alamat'] : 'N/A'; ?></td>
                 </tr>
                 <tr>
-                    <td>Tempat Lahir</td>
+                    <td>Nama Ayah</td>
                     <td>:</td>
-                    <td><?= $data['tempat_lahir'] ?></td>
+                    <td><?php echo isset($data_status['nama_ayah']) ? $data_status['nama_ayah'] : 'N/A'; ?></td>
+                </tr>
+                <tr>
+                    <td>Nama Ibu</td>
+                    <td>:</td>
+                    <td><?php echo isset($data_status['nama_ibu']) ? $data_status['nama_ibu'] : 'N/A'; ?></td>
+                </tr>
+                <tr>
+                    <td>Program Yang Diambil</td>
+                    <td>:</td>
+                    <td><?php echo isset($data_status['program']) ? $data_status['program'] : 'N/A'; ?></td>
+                </tr>
+                <tr>
+                    <td>Status Pendaftaran</td>
+                    <td>:</td>
+                    <td>
+                    <?php 
+                        if ($data_status['status'] == '1') {
+                            echo "<span class='badge badge-success'>Selamat Anda Diterima Sebagai Calon Santri</span>";
+                            echo "<br>";
+                            echo "<br>";
+                            echo "<a href='../../controller/generate_pdf.php?no_pendfawal=$data_status[no_pendfawal]' target='_blank' class='btn btn-info'>Cetak PDF</a>";
+                        } else if  ($data_status['status'] == '2'){
+                            echo "<span class='badge badge-danger'>Mohan Maaf, Anda Tidak Lolos Seleksi Sebagai Calon Santri</span>";
+                        } else {
+                            echo "<div class='alert alert-warning' role='alert'>Status : Sedang Dalam Proses Penilaian Panitia</div>";
+                        }   
+                    ?>
+                    </td>
                 </tr>
             </table>
-            <?php 
-                if ($data['status'] == '1') {
-                    echo "<div class='alert alert-warning' role='alert'>Status : Calon Santri</div>";
-                } else {
-                    echo "<div class='alert alert-warning' role='alert'>Status : Sedang Dalam Proses Penilaian Panitia</div>";
-                }
-            ?>
         </div>
     </div>
 </div>
